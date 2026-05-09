7ea51c64648b530de04b828405b729a098aa7ff6
kate <caterina.camerlengo@icloud.com>
2026-05-09 18:38:23 +0000

    1

--- cutter.py
+++ cutter.py
@@ -1,8 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+from openai import OpenAI
+import os
+client = OpenAI(
+    api_key=os.environ.get("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"),
+    base_url="https://api.groq.com/openai/v1",
+)
+# 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
+
+response = client.responses.create(
+    input="Explain the importance of fast language models",
+    model="openai/gpt-oss-20b",
+)
+print(response.output_text)
+
+
 def scrape():
-    url = 'https://www.example.com'
+    url = 'https://example.com'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     print(soup)
